83192af421962d6ed7b9a591f5a6d1258586b4c4
Vernaliz Gonzalez <vernaliz@Vernalizs-MBP.home>
2019-05-06 23:39:07 -0400

    changes

--- src/components/Context.js
+++ src/components/Context.js
@@ -10,61 +10,21 @@ function Context(props){
         <Row>
           <Col>
             <Card bg="danger" text="white" style={styles.cardStyle}>
-                <Card.Header>{props.heading}</Card.Header>
+                <Card.Header>Language: {props.heading}</Card.Header>
                 <Card.Body>
                   <input type="checkbox" className="form-check-input" id="exampleCheck1" /> 
-                <Card.Title>Title: {props.title}
+                <Card.Title>Subject: {props.title}
                 </Card.Title>
                 <Card.Text >
-                  Text: {props.text}
+                  Summary: {props.text}
                 </Card.Text>
                 </Card.Body>
                 </Card>
+                <br />
           </Col>
-          <br />
-          <br />
-                        
-                  {/* <Col> 
-                  <Card bg="warning" text="white" style={styles.cardStyle}>
-                          <Card.Header>JavaScript</Card.Header>
-                          <Card.Body>
-                            <Card.Title>How do you use it?</Card.Title>
-                            <Card.Text>
-                              Good question, simple answer. You can write JavaScript within the HTML page by using the script elements.
-                              As you progress in coding you'll learn that for cleaner, more readable code you use a separate file saved as a js file (milkshake).
-                              you take your current HTML which we will call iceCream.HTML and add a script tag referring to your Javascript file known as (milkshake.js).
-                              That simple you now have the necessary tool to take your iceCream(HTML) and turn it into a milkShake (javascript).
-
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                        </Col> */}
-                  {/* <Col> 
-                  <Card bg="info" text="white" style={styles.cardStyle}>
-                          <Card.Header>Javascript</Card.Header>
-                          <Card.Body>
-                            <Card.Title>Primary Card Title</Card.Title>
-                            <Card.Text>
-                              Some quick example text to build on the card title and make up the bulk
-                              of the card's content.
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                        </Col> */}
-                        <br />
-                </Row>
-              </Container>
-            {/* <Card bg="primary" text="white" style={{ width: '18rem' }}>
-            <Card.Header>Header</Card.Header>
-            <Card.Body>
-              <Card.Title>Primary Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk
-                of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card> */}
-          </div>
+        </Row>
+      </Container>
+    </div>
          );
     }
  
@@ -75,7 +35,7 @@ const styles= {
     width: '20rem', 
     height:'20rem', 
     fontSize: 20, 
-    overflowY:"scroll"
+    overflowY:"scroll",
     
 
   }
