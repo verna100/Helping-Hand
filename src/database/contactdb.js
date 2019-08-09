115691304a6815e9c8369473cb034a7839972d23
Vernaliz Gonzalez <vernaliz@Vernalizs-MacBook-Pro.local>
2019-08-08 21:48:53 -0400

    database connect

--- src/database/contactdb.js
+++ src/database/contactdb.js
@@ -14,83 +14,78 @@ var connection = mysql.createConnection({
   database: "helpinghands_db"
 });
 
+// This tells you when the db is connected and calls the create contact function
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
-});
-
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("connected as id " + connection.threadId + "\n");
-//     createProduct();
-//   });
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    createContact();
+  });
   
-//   function createProduct() {
-//     console.log("Inserting a new product...\n");
-//     var query = connection.query(
-//       "INSERT INTO products SET ?",
-//       {
-//         flavor: "Rocky Road",
-//         price: 3.0,
-//         quantity: 50
-//       },
-//       function(err, res) {
-//         console.log(res.affectedRows + " product inserted!\n");
-//         // Call updateProduct AFTER the INSERT completes
-//         updateProduct();
-//       }
-//     );
+  function createContact() {
+    console.log("Inserting a new contact...\n");
+    var query = connection.query(
+      "INSERT INTO contact_information SET ?",
+      {
+        full_name: "Tester Test",
+        email: "tester@email.com",
+        comm_sugg: "This is me testing the database connection"
+      },
+      function(err, res) {
+        console.log(res.affectedRows + " Contact inserted!\n");
+        // Call updateContact AFTER the INSERT completes
+        updateContact();
+      }
+    );
   
-//     // logs the actual query being run
-//     console.log(query.sql);
-//   }
+    // logs the actual query being run
+    console.log(query.sql);
+  }
   
-//   function updateProduct() {
-//     console.log("Updating all Rocky Road quantities...\n");
-//     var query = connection.query(
-//       "UPDATE products SET ? WHERE ?",
-//       [
-//         {
-//           quantity: 100
-//         },
-//         {
-//           flavor: "Rocky Road"
-//         }
-//       ],
-//       function(err, res) {
-//         console.log(res.affectedRows + " products updated!\n");
-//         // Call deleteProduct AFTER the UPDATE completes
-//         deleteProduct();
-//       }
-//     );
+  function updateContact() {
+    console.log("Updating all contacts...\n");
+    var query = connection.query(
+      "UPDATE contact_information SET ? WHERE ?",
+      [
+        {
+          email: "tester@email.com"
+        },
+        {
+          full_name: "Tester Test"
+        }
+      ],
+      function(err, res) {
+        console.log(res.affectedRows + " contact information updated!\n");
+        // Call deleteContact AFTER the UPDATE completes
+        deleteContact();
+      }
+    );
   
 //     // logs the actual query being run
-//     console.log(query.sql);
-//   }
+    console.log(query.sql);
+  }
   
-//   function deleteProduct() {
-//     console.log("Deleting all strawberry icecream...\n");
-//     connection.query(
-//       "DELETE FROM products WHERE ?",
-//       {
-//         flavor: "strawberry"
-//       },
-//       function(err, res) {
-//         console.log(res.affectedRows + " products deleted!\n");
-//         // Call readProducts AFTER the DELETE completes
-//         readProducts();
-//       }
-//     );
-//   }
+  function deleteContact() {
+    console.log("Deleting first contact...\n");
+    connection.query(
+      // "DELETE FROM contact_information WHERE ?",
+      {
+        id: 1
+      },
+      function(err, res) {
+        // console.log(res.affectedRows + " Contact deleted!\n");
+        // Call readContact AFTER the DELETE completes
+        readContact();
+      }
+    );
+  }
   
-//   function readProducts() {
-//     console.log("Selecting all products...\n");
-//     connection.query("SELECT * FROM products", function(err, res) {
-//       if (err) throw err;
-//       // Log all results of the SELECT statement
-//       console.log(res);
-//       connection.end();
-//     });
-//   }
+  function readContact() {
+    console.log("Selecting all contact_information...\n");
+    connection.query("SELECT * FROM contact_information", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      connection.end();
+    });
+  }
   
